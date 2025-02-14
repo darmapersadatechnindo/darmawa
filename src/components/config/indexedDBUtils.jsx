2979dc6ad2236ab7fc48e172faeb1614fa8cbbf3
Deni Darmayana <denidarmayana88@gmail.com>
2025-02-14 22:20:22 +0700

    indexDB

--- src/components/config/indexedDBUtils.jsx
+++ src/components/config/indexedDBUtils.jsx
@@ -4,20 +4,20 @@ import { openDB } from "idb";
 const dbPromise = openDB("WhatsAppMediaDB", 1, {
   upgrade(db) {
     if (!db.objectStoreNames.contains("media")) {
-      db.createObjectStore("media", { keyPath: "id" }); // Gunakan keyPath untuk indexing lebih rapi
+      db.createObjectStore("media", { keyPath: "id" }); 
     }
   },
 });
 
 export const saveMedia = async (mediaId, mediaBlob, mimetype) => {
   const db = await dbPromise;
-  await db.put("media", { blob: mediaBlob, mimetype }, mediaId);
+  await db.put("media", { id: mediaId, blob: mediaBlob, mimetype });
 };
 
 export const getMedia = async (mediaId) => {
   const db = await dbPromise;
   const data = await db.get("media", mediaId);
-  if (!data || !data.blob) return null; // Pastikan ada data
+  if (!data || !data.blob) return null; 
 
-  return new Blob([data.blob], { type: data.mimetype }); // Konversi ke Blob
+  return new Blob([data.blob], { type: data.mimetype }); 
 };
